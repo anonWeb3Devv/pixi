73245e59136a8f4591eb2e987207a24dd91d1488
Dev Anon <devanon79@gmail.com>
2024-09-26 12:13:10 -0400

    dino

--- src/components/dino/index.tsx
+++ src/components/dino/index.tsx
@@ -1,18 +1,71 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const PLAYER_HEIGHT = 60;
+const PLAYER_WIDTH = 40;
+const PLAYER_X = 140;
+const PLAYER_RIGHT_BORDER = PLAYER_X + PLAYER_WIDTH;
+
 const GAME_SPEED = 3.5;
 const JUMP_LIMIT = 120;
 const JUMP_SPEED = 10;
 const JUMP_KEYS = ["Space", "ArrowUp"];
 const OBSTACLE_SPAWN_RATE = 2000;
-
-const OBSTACLE_HEIGHT = 20;
+const OBSTACLE_WIDTH = 16;
 
 type Obstacle = {
   id: number;
   position: number;
+  img: string;
+  height: number;
 };
 
+const obstaclesImages = [
+  "./images/dino/obstacle_1.png",
+  "./images/dino/obstacle_2.png",
+];
+
+const obstaclesOptions = [
+  {
+    img: "./images/dino/obstacle_1.png",
+    height: 19,
+  },
+  {
+    img: "./images/dino/obstacle_2.png",
+    height: 26,
+  },
+];
+
+function getRandomObstacle() {
+  return obstaclesOptions[Math.floor(Math.random() * obstaclesImages.length)];
+}
+
+function getImage({
+  isMoving,
+  isJumping,
+  isGameOver,
+}: {
+  isMoving: boolean;
+  isJumping: boolean;
+  isGameOver: boolean;
+}) {
+  if (isGameOver) {
+    return "./images/dino/over.png";
+  }
+
+  if (isJumping) {
+    return "./images/dino/jump.png";
+  }
+
+  if (isMoving) {
+    return "./images/dino/moving.gif";
+  }
+
+  return "./images/dino/stationary.png";
+}
+
 export function Dino() {
+  const intervalRef = useRef<number | null>(null);
+
   const [scenarioPosition, setScenarioPosition] = useState(0);
   const [isJumping, setIsJumping] = useState(false);
   const [playerPositionY, setPlayerPositionY] = useState(3);
@@ -28,7 +81,9 @@ export function Dino() {
 
     const id = Math.random();
     const position = window.innerWidth + Math.floor(Math.random() * 1000);
-    setObstacles((prev) => [...prev, { id, position }]);
+
+    const obstacle = getRandomObstacle();
+    setObstacles((prev) => [...prev, { id, position, ...obstacle }]);
   }, [obstacles.length]);
 
   const handleKeyDown = useCallback(
@@ -76,7 +131,24 @@ export function Dino() {
         setIsFalling(false);
       }
     }
-  }, [isFalling, isJumping, playerPositionY]);
+
+    obstacles.forEach((obstacle) => {
+      if (
+        obstacle.position < PLAYER_RIGHT_BORDER - 10 &&
+        obstacle.position > PLAYER_X - OBSTACLE_WIDTH &&
+        playerPositionY < obstacle.height + 10
+      ) {
+        clearInterval(intervalRef.current!);
+        setIsGameOver(true);
+        setIsRunning(false);
+      }
+    });
+
+    if (isGameOver) {
+      clearInterval(intervalRef.current!);
+      return;
+    }
+  }, [isFalling, isGameOver, isJumping, obstacles, playerPositionY]);
 
   useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
@@ -85,44 +157,21 @@ export function Dino() {
     if (!isRunning) return;
 
     const interval = setInterval(gameLoop, 20);
-
-    if (isGameOver) {
-      clearInterval(interval);
-      return;
-    }
-
-    obstacles.forEach((obstacle) => {
-      if (
-        obstacle.position < 45 &&
-        obstacle.position > 20 &&
-        playerPositionY < OBSTACLE_HEIGHT + 10
-      ) {
-        clearInterval(interval);
-        setIsGameOver(true);
-      }
-    });
+    intervalRef.current = interval;
 
     return () => {
-      clearInterval(interval);
+      clearInterval(intervalRef.current!);
       window.removeEventListener("keydown", handleKeyDown);
       window.removeEventListener("keyup", handleKeyUp);
     };
-  }, [
-    gameLoop,
-    handleKeyDown,
-    handleKeyUp,
-    isGameOver,
-    isRunning,
-    obstacles,
-    playerPositionY,
-  ]);
+  }, [gameLoop, handleKeyDown, handleKeyUp, isRunning]);
 
   useEffect(() => {
     if (!isRunning) return;
 
     const interval = setInterval(generateObstacle, OBSTACLE_SPAWN_RATE);
 
-    if (!isRunning || isGameOver) {
+    if (isGameOver) {
       clearInterval(interval);
       return;
     }
@@ -161,14 +210,19 @@ export function Dino() {
       <div
         style={{
           position: "absolute",
-          width: "45px",
-          height: "45px",
+          width: PLAYER_WIDTH,
+          height: PLAYER_HEIGHT,
           zIndex: 1,
-          left: 10,
+          left: PLAYER_X,
           backgroundRepeat: "no-repeat",
-          backgroundSize: "100% 100%",
+          backgroundSize: "160% 100%",
+          backgroundPositionX: "-10px",
           bottom: playerPositionY,
-          backgroundImage: "url(./images/dino/dino-stationary.png)",
+          backgroundImage: `url(${getImage({
+            isMoving: isRunning,
+            isJumping: isJumping || isFalling,
+            isGameOver: isGameOver,
+          })})`,
         }}
       />
 
@@ -187,18 +241,19 @@ export function Dino() {
         }}
       />
 
+      {/* Obstacles */}
       {obstacles.map((obstacle) => (
         <div
           key={obstacle.id}
           style={{
             position: "absolute",
-            width: "20px",
-            height: `${OBSTACLE_HEIGHT}px`,
-            backgroundColor: "red",
+            border: "1px solid red",
             left: obstacle.position,
             bottom: 5,
           }}
-        />
+        >
+          <img src={obstacle.img} alt="obstacle" />
+        </div>
       ))}
     </div>
   );
